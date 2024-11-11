0214b3b9d3baea6b926fe8906da65af53cfbdba1
KRSL367 <krishalpudasaini12345@gmail.com>
2024-11-11 23:05:15 +0545

    Updating the login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useLoginUser } from '../hooks/useLoginData';
-import { Link, Navigate } from 'react-router-dom';
+import { Link} from 'react-router-dom';
 
 const LoginPage: React.FC = () => {
   const [username, setUsername] = useState('');
